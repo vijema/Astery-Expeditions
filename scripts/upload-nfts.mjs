7bafd3ae166122a39a4110f7f53362eeef273128
VIJEMA <vko.projects@gmail.com>
2024-03-24 18:22:24 +0500

    2024-03-24

--- scripts/upload-nfts.mjs
+++ scripts/upload-nfts.mjs
@@ -12,17 +12,21 @@ const sdk = ThirdwebSDK.fromPrivateKey(process.env.PRIVATE_KEY, "polygon", {
 
 (async () => {
     const drop = await sdk.getContract(
-      "0xf0Cd04492E937734a8d2CD1022824f90F5Ad43d6",
+      "0x5E743d0b65b6dB8Ee666d58f6cc978Eeac5b7547",
       "edition-drop"
     );
   
     try {
       await drop.createBatch([
-              "https://my-json-server.typicode.com/vijema/Astery-Expeditions/expeditions/8"
+              "https://my-json-server.typicode.com/vijema/Astery-Expeditions/expeditions/3",
+              "https://my-json-server.typicode.com/vijema/Astery-Expeditions/expeditions/4"
+
+              // "https://my-json-server.typicode.com/vijema/Astery-Expeditions/expeditions/2"
 
               // 1) Меняем индекс в ссылке выше
               // 2) Добавляем новую мету в db.json и коммитим - ждем апдейта по ссылке
               // 3) Запускаем node scripts/upload-nfts.mjs 
+              // https://www.npoint.io/docs/a42017eb51596187e46e
               
 
       ]);
