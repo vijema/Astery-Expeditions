e4ee1b17d244de5dfaab5656cd8e3fca81ddb08c
VIJEMA <vko.projects@gmail.com>
2024-03-24 19:18:32 +0500

    2024-03-24-3

--- scripts/upload-nfts.mjs
+++ scripts/upload-nfts.mjs
@@ -12,17 +12,15 @@ const sdk = ThirdwebSDK.fromPrivateKey(process.env.PRIVATE_KEY, "polygon", {
 
 (async () => {
     const drop = await sdk.getContract(
-      "0xf0Cd04492E937734a8d2CD1022824f90F5Ad43d6", // my-json-server
-      // "0x5E743d0b65b6dB8Ee666d58f6cc978Eeac5b7547", //Npoint
+      // "0xf0Cd04492E937734a8d2CD1022824f90F5Ad43d6", // my-json-server
+      "0x5E743d0b65b6dB8Ee666d58f6cc978Eeac5b7547", //Npoint
       "edition-drop"
     );
   
     try {
       await drop.createBatch([
-              "https://my-json-server.typicode.com/vijema/Astery-Expeditions/expeditions/3",
-              "https://my-json-server.typicode.com/vijema/Astery-Expeditions/expeditions/4",
-              "https://my-json-server.typicode.com/vijema/Astery-Expeditions/expeditions/5",
-              "https://my-json-server.typicode.com/vijema/Astery-Expeditions/expeditions/6"
+              "https://api.npoint.io/a42017eb51596187e46e/expeditions/13",
+              "https://api.npoint.io/a42017eb51596187e46e/expeditions/14"
 
               // "https://my-json-server.typicode.com/vijema/Astery-Expeditions/expeditions/2"
               // "https://api.npoint.io/a42017eb51596187e46e/expeditions/8"
